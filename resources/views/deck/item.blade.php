295417de16a1bc2e1bfebb9e89af145f55fd2175
zack <zigiska@gmail.com>
2019-03-14 14:22:07 +0300

    rediculous fall

--- resources/views/deck/item.blade.php
+++ resources/views/deck/item.blade.php
@@ -4,25 +4,25 @@
     <h2>{{$deck->name}}</h2>
 
 <div id="setEditTool" class="mb-1">
-    <button class="btn btn-info" data-toggle="collapse" 
+    <button class="btn btn-info" data-toggle="collapse"
         data-target="#addEntitiesForm" aria-expanded="false" aria-controls="#addEntitiesForm">
         <i class="fa fa-plus"></i>
         {{__('label.add')}}
         <i class="fa fa-caret-down"></i>
     </button>
-    <a href='#' class="btn btn-success"><i class="fa fa-rocket"></i> {{__('label.test')}}</a>
-    <a href='#' class="btn btn-success"><i class="fa fa-reply-all"></i> {{__('label.revers')}}</a>
+    <a href='{{route('test', [$id = $deck->id])}}' class="btn btn-success"><i class="fa fa-rocket"></i> {{__('label.test')}}</a>
+    <a href='{{route('test.revers', [$id = $deck->id])}}' class="btn btn-success"><i class="fa fa-reply-all"></i> {{__('label.revers')}}</a>
     <a href='#' class="btn btn-outline-info" data-toggle="modal" data-target="#joinForm" aria-expanded="false" aria-controls="#joinForm">
         <i class="fa fa-object-ungroup"></i>
         {{__('label.joinTo')}}</a>
 </div>
 <div class='collapse' id='addEntitiesForm'>
         <div class="card col-md-6 my-3 mx-auto bg-paleorange p-0">
-            <form method="POST"action="{{route('pair.store', [$id = $deck->id])}}"> 
+            <form method="POST"action="{{route('pair.store', [$id = $deck->id])}}">
             @csrf
                 <div class="card-body">
 
-                    <div  id="entityAddTemplate" class="d-none" data-identy="Group">   
+                    <div  id="entityAddTemplate" class="d-none" data-identy="Group">
                         <div class="form-group">
                             <label>{{__('label.key')}}</label>
                             <input type="text" class="form-control" data-identy="Value">
@@ -32,21 +32,21 @@
                         </div>
                         <div class="form-group">
                             <a href="#" class="btn btn-outline-danger btn-sm d-none" data-identy="Del"><i class="far fa-trash-alt"></i> {{__('label.delete')}}</a>
-                            
+
                         </div>
                     </div>
-                
+
                 </div>
                 <div class="card-footer d-flex justify-content-between">
                     <div class="btn-group">
                         <button type="button" class="btn btn-outline-info"  data-toggle="collapse" data-target="#addEntitiesForm" aria-expanded="false" aria-controls="#addEntitiesForm">
                             {{__('label.hide')}}
-                            <i class="fa fa-caret-up"></i> 
+                            <i class="fa fa-caret-up"></i>
                         </button>
-                        <button type="button" class="btn btn-info" id="addPairBtn"> 
+                        <button type="button" class="btn btn-info" id="addPairBtn">
                             <i class="fa fa-plus"></i>
                             {{__('deck.morePair')}}
-                                
+
                         </button>
                     </div>
                     <button type="submit" class="btn btn-primary" id="formSubmit">{{__('label.add')}}</button>
@@ -82,19 +82,19 @@ Nothing
                 <div class="btn-group">
                     <a href='{{route('pair.edit', [$id = $pair->id])}}' class="btn btn-outline-primary btn-sm">
                         <span class="fas fa-pen"></span></a>
-                    <a href="#" class="btn btn-outline-danger btn-sm" data-toggle="modal" 
+                    <a href="#" class="btn btn-outline-danger btn-sm" data-toggle="modal"
                         data-target="#dellingForm"
                         role="delEntity"
                         data-pair-id="{{$pair->id}}"
-                        data-counter = "{{$counter}}"><span class="far fa-trash-alt"></a>                        
+                        data-counter = "{{$counter}}"><span class="far fa-trash-alt"></a>
                 </div>
-                
+
                 </td>
             </tr>
         @endforeach
         </tbody>
     </table>
-@endif  
+@endif
 </div>
 
 <!-- JoinTo modal form -->
@@ -117,7 +117,7 @@ Nothing
                 @foreach (\App\Models\Subject::where('user_id', \Auth::id())->with('decks')->get() as $subject)
                 <optgroup label="{{$subject->name}}">
                     @foreach($subject->decks as $deck)
-                    <option value="{{$deck->id}}">                                   
+                    <option value="{{$deck->id}}">
                         {{$deck->name}}
                     </option>
                     @endforeach
@@ -166,11 +166,11 @@ Nothing
         var el = $("#entityAddTemplate").clone();
         el.insertBefore('#entityAddTemplate').removeClass('d-none').attr('id', AddEntity.Count);
         el.find('input[data-identy=Value]').attr('name', 'Value[' + AddEntity.Count + ']');
-        el.find('input[data-identy=Link]').attr('name', 'Link[' + AddEntity.Count + ']');       
+        el.find('input[data-identy=Link]').attr('name', 'Link[' + AddEntity.Count + ']');
         el.find('[data-identy=Del]').on('click', function(){
             $(this).closest('[data-identy=Group]').remove();
             if($('[data-identy=Del]').length == 2) $('[data-identy=Del]').addClass('d-none');
-        }); 
+        });
         var $count = $('[data-identy=Del]').length
         if($count > 2) $('[data-identy=Del]').removeClass('d-none');
     }
@@ -178,10 +178,10 @@ Nothing
 
 $(function(){
     AddEntity.CloneTemplate();
-    $('#addPairBtn').on('click', function(){       
+    $('#addPairBtn').on('click', function(){
         AddEntity.Count +=1;
         AddEntity.CloneTemplate();
-     
+
     });
 
     $("#dellingForm").on('show.bs.modal', function (event) {
