9192a919b8c5569c93a4a57e895c9b93b7d20625
zack <zigiska@gmail.com>
2019-03-14 13:50:38 +0300

    About page on en

--- resources/views/info/about.blade.php
+++ resources/views/info/about.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app', ['title' => __('label.about')])
 @section('content')
-<div class="container-fluid py-2">
+<div class="container-fluid">
     <div class="chapter container">
         <h1>{{__('label.about')}}</h1>
         <img src="https://farm7.staticflickr.com/6108/6238256033_2962350630_b.jpg" max-width="30%" width="300px" align="right">
@@ -9,28 +9,31 @@
         <p>All you need is a browser and Internet.</p>
     </div>
 </div>
-<div class="container-fluid bg-primary py-2">
+<div class="container-fluid bg-verylightgrey">
     <div class="chapter container">
         <h2>Conception</h2>
-        <p>This web application help you to learn or get remembered any knowledge you want. Foreign words, science definitions, or just links in a mind map - all this things remembering become easier with conception of two-sided cards. This app helps you to make decks from virtual cards.  </p>
-        <p>Then you can learn or test your knowledges from different devices. Fast test repeating from phone in any comfortable situation is avaliable. Either a thorough study with the computer.</p>
-        <p>All you need is a browser and Internet.</p>
+        <p>You may choose your own rithm of study. But I advise you to memorize in four stages:</p>
+        <p><ol>
+            <li>You have imagine two associations with each side of the card and then merge them to one form in your mind. At the same time loudly say for yourself text from the card.</li>
+            <li>Actually optionl. You may repeat learning before the night sleep. Now you got this information in short-term memory.</li>
+            <li>To get it to long-term memory you have to test this knowledge four days for one time each and a week later one time. </li>
+            <li>Now you know it good. But you can forget it if will not repeat testing time to time. A month later or year it depend on you but you have to repeat. Good reason to test foreign is a travel for exemple.</li>
+        </ol></p>
     </div>
 </div>
-<div class="container-fluid bg-light py-2">
+<div class="container-fluid">
     <div class="chapter container">
-        <h2>Conception</h2>
-        <p>This web application help you to learn or get remembered any knowledge you want. Foreign words, science definitions, or just links in a mind map - all this things remembering become easier with conception of two-sided cards. This app helps you to make decks from virtual cards.  </p>
-        <p>Then you can learn or test your knowledges from different devices. Fast test repeating from phone in any comfortable situation is avaliable. Either a thorough study with the computer.</p>
-        <p>All you need is a browser and Internet.</p>
+        <h2>First things firs</h2>
+        <p>At first you need to do is to create your own subject for study. It can be Belarussion-English dictionary, words from book or song, the elements of the periodic table, etc.</p>
+        <p>At second you have to create and organaze your deck. </p>
+        <p>After filling deck you must learn and test.</p>
     </div>
 </div>
-<div class="container-fluid bg-secondary py-2">
+<div class="container-fluid bg-lightblue">
     <div class="chapter container">
-        <h2>Conception</h2>
-        <p>This web application help you to learn or get remembered any knowledge you want. Foreign words, science definitions, or just links in a mind map - all this things remembering become easier with conception of two-sided cards. This app helps you to make decks from virtual cards.  </p>
-        <p>Then you can learn or test your knowledges from different devices. Fast test repeating from phone in any comfortable situation is avaliable. Either a thorough study with the computer.</p>
-        <p>All you need is a browser and Internet.</p>
+        <h2>Summary</h2>
+        <p>At the beginig I was making the Memorizer for my self. A wanted to make remembering proccess easer. I hope I did it.</p>
+        <p>Memorizer is not finished. Many wonder upgrades will comming in future.</p>
     </div>
 </div>
 @endsection
