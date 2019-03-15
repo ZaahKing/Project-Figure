2b37517b2b64bcebc74d0524652aa7ca96088704
zack <zigiska@gmail.com>
2019-03-15 10:53:33 +0300

    L1: based on test page

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,27 +7,39 @@ use \App\Models\Pair;
 
 class TestController extends Controller
 {
-    public function Test($id) 
-    { 
+    public function Test($id)
+    {
         $data = Pair::where('user_id', \Auth::id())->where('deck_id', $id)->get();
-        return view('test.test', compact('data')); 
+        return view('test.test', compact('data'));
     }
 
-    public function Revers($id) 
-    { 
+    public function Revers($id)
+    {
         $data = Pair::where('user_id', \Auth::id())->where('deck_id', $id)->get();
-        return view('test.test', compact('data'))->with(['revers'=>true]); 
+        return view('test.test', compact('data'))->with(['revers'=>true]);
     }
 
-    public function MassTest(Request $request) 
-    { 
+    public function MassTest(Request $request)
+    {
         $data = Pair::where('user_id', \Auth::id())->whereIn('deck_id', $request->input('id'))->get();
-        return view('test.test', compact('data')); 
+        return view('test.test', compact('data'));
     }
 
-    public function MassRevers(Request $request) 
-    { 
+    public function MassRevers(Request $request)
+    {
         $data = Pair::where('user_id', \Auth::id())->whereIn('deck_id', $request->input('id'))->get();
-        return view('test.test', compact('data'))->with(['revers'=>true]); 
+        return view('test.test', compact('data'))->with(['revers'=>true]);
+    }
+
+    public function MassLearning(Request $request)
+    {
+        $data = Pair::where('user_id', \Auth::id())->whereIn('deck_id', $request->input('id'))->get();
+        return view('test.test', compact('data'));
+    }
+
+    public function Learning($id)
+    {
+        $data = Pair::where('user_id', \Auth::id())->where('deck_id', $id)->get();
+        return view('test.test', compact('data'))->with(['revers'=>true]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,13 @@ Route::get('/me', 'InfoController@Portfolio')->name('me');
 Route::prefix('subject')->middleware(['auth'])->group(function(){
     Route::get('/', 'SubjectController@Index')->name('subject.list');
     Route::post('/add', 'SubjectController@Create')->name('subject.add');
-    Route::post('/delete', 'SubjectController@Delete')->name('subject.delete');   
-    Route::get('/edit/{id}', 'SubjectController@Edit')->name('subject.edit');   
-    Route::post('/edit/{id}', 'SubjectController@Update')->name('subject.update');   
+    Route::post('/delete', 'SubjectController@Delete')->name('subject.delete');
+    Route::get('/edit/{id}', 'SubjectController@Edit')->name('subject.edit');
+    Route::post('/edit/{id}', 'SubjectController@Update')->name('subject.update');
 });
 
 Route::prefix('deck')->middleware(['auth'])->group(function(){
-    Route::get('/', 'DeckController@Index')->name('deck.list');   
+    Route::get('/', 'DeckController@Index')->name('deck.list');
     Route::get('/add', 'DeckController@Create')->name('deck.create');
     Route::post('/add', 'DeckController@Store')->name('deck.store');
     Route::post('/action', 'DeckController@Action')->name('deck.action');
@@ -34,7 +34,7 @@ Route::prefix('deck')->middleware(['auth'])->group(function(){
 });
 
 Route::prefix('pair')->middleware(['auth'])->group(function(){
-    Route::get('/', 'PairController@Index')->name('pair.list');   
+    Route::get('/', 'PairController@Index')->name('pair.list');
     Route::post('/add/deck-{id}', 'PairController@Store')->name('pair.store');
     Route::get('/edit/{id}', 'PairController@Edit')->name('pair.edit');
     Route::post('/update/{id}', 'PairController@Update')->name('pair.update');
@@ -47,4 +47,9 @@ Route::prefix('test')->middleware(['auth'])->group(function(){
     Route::post('/revers', 'TestController@MassRevers')->name('test.mass.revers');
     Route::get('/{id}', 'TestController@Test')->name('test');
     Route::get('/revers/{id}', 'TestController@Revers')->name('test.revers');
+});
+
+Route::prefix('learning')->middleware(['auth'])->group(function(){
+    Route::post('/', 'TestController@MassLearning')->name('learning.mass');
+    Route::get('/{id}', 'TestController@Learning')->name('learning');
 });
